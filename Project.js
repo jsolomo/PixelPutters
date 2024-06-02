1e7e1a263862d352938c92da70687468aba20fb0
marcokfung <47341733+marcokfung@users.noreply.github.com>
2024-06-01 22:08:00 -0700

    Added Files

--- Project.js
+++ Project.js
@@ -99,7 +99,7 @@ export class Assignment3 extends Scene {
             {ambient: 1, diffusivity: 1, color: hex_color("#ADD8E6")}),
             scoreboard: new Material(new defs.Phong_Shader(),
             {ambient: 1, diffusivity: 0, specularity: 0, color: hex_color("#FFE36E")}),
-            texture: new Material(new Textured_Phong(), {
+            sky: new Material(new Textured_Phong(), {
                 color: color(0, 0, 0, 1),
                 ambient: 1,
                 texture: new Texture("assets/sky.jpg")
@@ -252,7 +252,7 @@ export class Assignment3 extends Scene {
         this.draw_tree(context, program_state);
         this.draw_fish(context, program_state, t, -100, 0, 350, 0);
         let sky_transformation = Mat4.inverse(program_state.camera_inverse).times(Mat4.scale(900,400,900));
-        this.shapes.cube.draw(context, program_state, sky_transformation, this.materials.texture);
+        this.shapes.cube.draw(context, program_state, sky_transformation, this.materials.sky);
 
         // Generate All Targets
         /*for (let i = 0; i < this.target_coords.length; i++){
@@ -373,7 +373,7 @@ export class Assignment3 extends Scene {
             let z_aim = Math.sin(-this.turn_angle + off_set) * bar_dist;
 
             this.power_angle = -Math.PI/4 * Math.cos((Math.PI) * t) + Math.PI/4;
-            const bar_transform = Mat4.identity().times(Mat4.inverse(program_state.camera_inverse).times(Mat4.translation(-3.48, 1, -5.25))).times(Mat4.scale(0.25,1,0.25));
+            const bar_transform = Mat4.identity().times(Mat4.inverse(program_state.camera_inverse).times(Mat4.translation(-3.48, 1, -5.27))).times(Mat4.scale(0.25,1,0.25));
             this.shapes.bar.draw(context, program_state, bar_transform, this.materials.bar);
             const picker_transform = Mat4.identity().times((Mat4.translation(0,-0.9* Math.cos(Math.PI*t) + 0.9, 0).times(Mat4.inverse(program_state.camera_inverse).times(Mat4.translation(-3.48, 0.1, -5.25)).times(Mat4.scale(0.3,0.06,0.2)))));
             this.shapes.bar.draw(context, program_state, picker_transform, this.materials.pointer);
